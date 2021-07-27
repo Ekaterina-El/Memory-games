47d705639635e46c73f53e285061eacb61d6faad
Ekaterina-El <hackerrussia@yandex.ru>
2021-07-27 13:13:51 +0500

    Added naviagation

--- app/src/main/java/ka/el/memorygames/ui/MenuFragment.kt
+++ app/src/main/java/ka/el/memorygames/ui/MenuFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import ka.el.memorygames.R
 
 class MenuFragment : Fragment() {
@@ -14,5 +16,15 @@ class MenuFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_menu, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        (view.findViewById(R.id.btn_play) as Button).setOnClickListener { play() }
+    }
+
+    private fun play() {
+        findNavController().navigate(R.id.action_menuFragment_to_playgroundFragment)
     }
 }
