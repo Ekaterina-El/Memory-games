136c65b68d9a1ea15ed35d6b4bed334b2cae182a
Ekaterina-El <hackerrussia@yandex.ru>
2021-07-27 18:25:48 +0500

    Added animation for rotate card

--- app/src/main/java/ka/el/memorygames/ui/playground/list/CardViewHolder.kt
+++ app/src/main/java/ka/el/memorygames/ui/playground/list/CardViewHolder.kt
@@ -1,7 +1,9 @@
 package ka.el.memorygames.ui.playground.list
 
+import android.animation.ObjectAnimator
 import android.view.View
 import android.widget.ImageView
+import androidx.core.animation.doOnEnd
 import androidx.recyclerview.widget.RecyclerView
 import ka.el.memorygames.R
 import ka.el.memorygames.ui.playground.CardModel
@@ -10,17 +12,33 @@ class CardViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     private val image: ImageView = itemView.findViewById(R.id.playground_item_image)
     private lateinit var viewModel: CardModel
 
+    private val animationStart1 = ObjectAnimator.ofFloat(itemView, "rotationY", 0.0f, 90f)
+    private val animationStart2 = ObjectAnimator.ofFloat(itemView, "rotationY", 90f, 180f)
+
+    private val animationEnd1 = ObjectAnimator.ofFloat(itemView, "rotationY", 180f, 90f)
+    private val animationEnd2 = ObjectAnimator.ofFloat(itemView, "rotationY", 90f, 0f)
+
     fun bindView(viewModel: CardModel) {
         this.viewModel = viewModel
+
+        animationStart1.doOnEnd {
+            image.setImageDrawable(viewModel.imageResId)
+            animationStart2.start()
+        }
+
+        animationEnd1.doOnEnd {
+            image.setImageResource(R.drawable.question)
+            animationEnd2.start()
+        }
     }
 
     fun openCard() {
-        image.setImageDrawable(viewModel.imageResId)
+        animationStart1.start()
         viewModel.isOpened = true
     }
 
     fun closeCard() {
-        image.setImageResource(R.drawable.question)
+        animationEnd1.start()
         viewModel.isOpened = false
     }
 }
